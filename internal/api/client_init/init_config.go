6f52fe73c17bb76e5ab23b94ef01d85b5fb01132
Janly <jjjachyty@163.com>
2023-04-13 16:51:31 +0800

    修改版本检查api 去掉token校验

--- internal/api/client_init/init_config.go
+++ internal/api/client_init/init_config.go
@@ -51,14 +51,14 @@ func GetClientInitConfig(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"errCode": 400, "errMsg": err.Error()})
 		return
 	}
-	log.NewInfo(req.OperationID, utils.GetSelfFuncName(), "req: ", req)
-	err, _ := token_verify.ParseTokenGetUserID(c.Request.Header.Get("token"), req.OperationID)
-	if err != nil {
-		errMsg := "ParseTokenGetUserID failed " + err.Error() + c.Request.Header.Get("token")
-		log.NewError(req.OperationID, errMsg, errMsg)
-		c.JSON(http.StatusInternalServerError, gin.H{"errCode": 500, "errMsg": errMsg})
-		return
-	}
+	// log.NewInfo(req.OperationID, utils.GetSelfFuncName(), "req: ", req)
+	// err, _ := token_verify.ParseTokenGetUserID(c.Request.Header.Get("token"), req.OperationID)
+	// if err != nil {
+	// 	errMsg := "ParseTokenGetUserID failed " + err.Error() + c.Request.Header.Get("token")
+	// 	log.NewError(req.OperationID, errMsg, errMsg)
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"errCode": 500, "errMsg": errMsg})
+	// 	return
+	// }
 	config, err := imdb.GetClientInitConfig()
 	if err != nil {
 		log.NewError(req.OperationID, utils.GetSelfFuncName(), err.Error())
