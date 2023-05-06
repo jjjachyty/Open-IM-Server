4a49b517009fbcfecf8050a06759fdcd0ad744e3
Janly <jjjachyty@163com>
2023-05-06 11:39:12 +0800

    1

--- internal/api/auth/agora.go
+++ internal/api/auth/agora.go
@@ -42,12 +42,12 @@ func generateRtcToken(int_uid uint32, channelName string, role rtctokenbuilder.R
 	// AccessToken2 过期的时间，单位为秒。
 	// 当 AccessToken2 过期但权限未过期时，用户仍在频道里并且可以发流，不会触发 SDK 回调。
 	// 但一旦用户和频道断开连接，用户将无法使用该 Token 加入同一频道。请确保 AccessToken2 的过期时间晚于权限过期时间。
-	tokenExpireTimeInSeconds := uint32(30)
+	tokenExpireTimeInSeconds := uint32(2 * 60 * 60)
 	// 权限过期的时间，单位为秒。
 	// 权限过期30秒前会触发 token-privilege-will-expire 回调。
 	// 权限过期时会触发 token-privilege-did-expire 回调。
 	// 为作演示，在此将过期时间设为 40 秒。你可以看到客户端自动更新 Token 的过程。
-	privilegeExpireTimeInSeconds := uint32(40)
+	privilegeExpireTimeInSeconds := uint32(2*60*60 + 10)
 
 	return rtctokenbuilder.BuildTokenWithUid(appID, appCertificate, channelName, int_uid, role, tokenExpireTimeInSeconds, privilegeExpireTimeInSeconds)
 
