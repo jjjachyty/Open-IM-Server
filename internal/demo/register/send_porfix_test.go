36c99f488a9ce55a0104f3e1115985301f4c6894
serv_luxun <serv_luxun@yf-work.top>
2023-05-30 21:26:38 +0800

    1

--- internal/demo/register/send_porfix_test.go
+++ internal/demo/register/send_porfix_test.go
@@ -1,55 +1,77 @@
 package register
 
 import (
-	"encoding/base64"
-	"net/mail"
+	"fmt"
 	"net/smtp"
-	"strings"
 	"testing"
 )
 
 func TestSendMail(t *testing.T) {
+	// Sender data.
+	from := "mail"
+	password := "pushimmail"
 
-	err := SendMail("127.0.0.1:25", (&mail.Address{"from name", "from@example.com"}).String(), "Email Subject", "message body", []string{(&mail.Address{"to name", "1942056324@qq.com"}).String()})
-	panic(err)
+	// Receiver email address.
+	to := []string{
+		"1942056324@qq.com",
+	}
+
+	// smtp server configuration.
+	smtpHost := "127.0.0.1"
+	smtpPort := "25"
+
+	// Message.
+	message := []byte("This is a test email message.")
+
+	// Authentication.
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// Sending email.
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Email Sent Successfully!")
 }
-func SendMail(addr, from, subject, body string, to []string) error {
-	r := strings.NewReplacer("\r\n", "", "\r", "", "\n", "", "%0a", "", "%0d", "")
 
-	c, err := smtp.Dial(addr)
-	if err != nil {
-		return err
-	}
-	defer c.Close()
-	if err = c.Mail(r.Replace(from)); err != nil {
-		return err
-	}
-	for i := range to {
-		to[i] = r.Replace(to[i])
-		if err = c.Rcpt(to[i]); err != nil {
-			return err
-		}
-	}
+// func SendMail(addr, from, subject, body string, to []string) error {
+// 	r := strings.NewReplacer("\r\n", "", "\r", "", "\n", "", "%0a", "", "%0d", "")
 
-	w, err := c.Data()
-	if err != nil {
-		return err
-	}
+// 	c, err := smtp.Dial(addr)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer c.Close()
+// 	if err = c.SendMail(r.Replace(from)); err != nil {
+// 		return err
+// 	}
+// 	for i := range to {
+// 		to[i] = r.Replace(to[i])
+// 		if err = c.Rcpt(to[i]); err != nil {
+// 			return err
+// 		}
+// 	}
 
-	msg := "To: " + strings.Join(to, ",") + "\r\n" +
-		"From: " + from + "\r\n" +
-		"Subject: " + subject + "\r\n" +
-		"Content-Type: text/html; charset=\"UTF-8\"\r\n" +
-		"Content-Transfer-Encoding: base64\r\n" +
-		"\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
+// 	w, err := c.Data()
+// 	if err != nil {
+// 		return err
+// 	}
 
-	_, err = w.Write([]byte(msg))
-	if err != nil {
-		return err
-	}
-	err = w.Close()
-	if err != nil {
-		return err
-	}
-	return c.Quit()
-}
+// 	msg := "To: " + strings.Join(to, ",") + "\r\n" +
+// 		"From: " + from + "\r\n" +
+// 		"Subject: " + subject + "\r\n" +
+// 		"Content-Type: text/html; charset=\"UTF-8\"\r\n" +
+// 		"Content-Transfer-Encoding: base64\r\n" +
+// 		"\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
+
+// 	_, err = w.Write([]byte(msg))
+// 	if err != nil {
+// 		return err
+// 	}
+// 	err = w.Close()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return c.Quit()
+// }
